e7bff0544a18c490e989573a2ae01a4e272b6ef6
terry-lws <terrylam299@gmail.com>
2023-04-10 03:05:30 +1000

    Add heading, change bgcolor to #efebe4, change color to #383842

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Text, Heading, VStack, Box, Image } from "@chakra-ui/react";
-import { Flex } from '@chakra-ui/react'
-//import { Container } from '@chakra-ui/react'
+import { Text, Heading, VStack, Box, Image, Flex } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const Bio = () => {
     return (
         <FullScreenSection
             id="bio-section"
-            backgroundColor="white"
+            backgroundColor="#efebe4"
+            color="#383842"
             px={8}
             py={20}
             alignItems="flex-start"
@@ -16,47 +15,38 @@ const Bio = () => {
             spacing={8}
         >
             <VStack spacing={5} >
-                <Flex gap={10} wrap={{ base: "wrap", lg: "nowrap" }} justifyContent="center">
-                    <Box w={{ base: "none", lg: '60%' }} p={2}>
-                        <Heading pb={3} as="h1">
-                            About Me
-                        </Heading>
-                        <Text py={3}>
-                            Hi there! My name is Terry Lam and I am a post-graduate computer science student at Monash University
-                            and a volunteer front-end web developer at a not-for-profit organisation.
-                        </Text>
-                        {/* <Text py={3}>
-                            In my free time, I enjoy indulging in a variety of hobbies that help me recharge and relax.
-                            I love staying active, and I try to maintain a regular exercise routine to keep myself physically and mentally fit.
-                            I enjoy hiking and cycling, which not only keep me fit but also help me explore new places and enjoy nature.
-                        </Text> */}
-                        <Text py={3}>
-                            During my Bachelor of Information Technology education in Australia, I was exposed to many fields of the IT industry,
-                            such as, machine learning, software engineering, and ICT project management. However, nothing interested me as much as web development did.
-                            I have always been fascinated by the intersection of technology and design, and web development is the perfect field to bring those two together.
-                        </Text>
-                        <Text py={3}>
-                            Throughout my academic studies, I have taken courses in web design and programming, which have helped me develop a strong foundation in HTML, CSS, and JavaScript,
-                            along with other web development frameworks and libraries.
-                            I am currently pursuing a professional certificate course in front-end web development provided by Meta to equip myself with industry-standard skills and prepare myself for a job-ready environment.
-                        </Text>
-                        <Text py={3}>
-                            Aside from web development, I have also acquired knowledge in various programming languages such as Python and Java,
-                            as well as ICT project management methodologies and approaches.
-                        </Text>
-                        <Text py={3}>
-                            My short-term goal is to secure a position as a front-end web developer,
-                            which will allow me to gain real-world industry experience and expand my technical and workplace knowledge.
-                            In the long term, I aim to further develop my skills by exposing myself to back-end web development and eventually become a full-stack developer.
-                        </Text>
-                    </Box>
+                <Heading as='h1'>
+                    Who Am I?
+                </Heading>
+                <Flex
+                    justifyContent='center'
+                    justifyItems='center'
+                    alignItems='center'
+                    gap={9}
+                    >
                     <Box
-                        w={{ base: "none", lg: '40%' }}
+                        //w={{ base: "none", lg: '40%' }}
+                        w='50%'
                         p={2}
-                        display="grid"
                         justifyContent="center"
                         alignItems="center">
-                        {/* <Image src={require("../images/pfp.jpeg")} /> */}
+                        <Image src={require("../images/img5.jpeg")} border='solid gray 2px' p='10px' boxShadow=' 5px 10px #888888' />
+                    </Box>
+                    <Box w='50%'>
+                        <Text py={3}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+                        </Text>
+
+
+                        <Text py={3}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+                        </Text>
+                        <Text py={3}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+                        </Text>
                     </Box>
                 </Flex>
             </VStack>
