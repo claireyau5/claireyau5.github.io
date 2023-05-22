89566946e0b0949f5417deb9af7a5342ed7793bd
terry-lws <terrylam299@gmail.com>
2023-05-23 05:30:40 +1000

    Add content

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -3,55 +3,77 @@ import { Text, Heading, VStack, Box, Image, Flex } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const Bio = () => {
-    return (
-        <FullScreenSection
-            id="bio-section"
-            backgroundColor="#efebe4"
-            color="#383842"
-            px={8}
-            py={20}
-            alignItems="flex-start"
-            justifyContent="center"
-            spacing={8}
+  return (
+    <FullScreenSection
+      id="bio-section"
+      backgroundColor="#efebe4"
+      color="#383842"
+      px={8}
+      py={20}
+      alignItems="flex-start"
+      justifyContent="center"
+      spacing={8}
+    >
+      <VStack spacing={10}>
+        <Heading as="h1">Who Am I?</Heading>
+        <Flex
+          justifyContent="center"
+          justifyItems="center"
+          alignItems="center"
+          gap={20}
         >
-            <VStack spacing={5} >
-                <Heading as='h1'>
-                    Who Am I?
-                </Heading>
-                <Flex
-                    justifyContent='center'
-                    justifyItems='center'
-                    alignItems='center'
-                    gap={9}
-                    >
-                    <Box
-                        //w={{ base: "none", lg: '40%' }}
-                        w='50%'
-                        p={2}
-                        justifyContent="center"
-                        alignItems="center">
-                        <Image src={require("../images/img5.jpeg")} border='solid gray 2px' p='10px' boxShadow=' 5px 10px #888888' />
-                    </Box>
-                    <Box w='50%'>
-                        <Text py={3}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-
-                        </Text>
-
-
-                        <Text py={3}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-
-                        </Text>
-                        <Text py={3}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-
-                        </Text>
-                    </Box>
-                </Flex>
-            </VStack>
-        </FullScreenSection>
-    )
+          <Box
+            //w={{ base: "none", lg: '40%' }}
+            w="50%"
+            p={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Image
+              src={require("../images/img5.jpeg")}
+              border="solid gray 2px"
+              p="10px"
+              boxShadow=" 5px 10px #888888"
+            />
+          </Box>
+          <Box w="50%">
+            <Text py={3}>
+              Claire Yau is a driven psychology enthusiast, passionate about
+              understanding the complexities of the human mind and the impact of
+              behavior on mental health. Her educational background includes a
+              Bachelor of Psychology (Hons) degree from the Australian National
+              University (ANU), where she gained a comprehensive understanding
+              of the theories and applications of psychology.
+            </Text>
+            <Text py={3}>
+              Currently working as a Patient Journey Coordinator at the
+              Melbourne Psychiatry Centre, Claire is dedicated to providing
+              patients with the resources and support they need to navigate the
+              healthcare system and receive the best possible care. Her work has
+              inspired her to pursue a career as a clinical psychologist, where
+              she hopes to work directly with patients to provide evidence-based
+              treatments and interventions.
+            </Text>
+            <Text py={3}>
+              As an active volunteer with organizations like Raise Foundation
+              and Lifeline, Claire puts her knowledge into practice by serving
+              as a mentor and crisis supporter. Her commitment to promoting
+              mental health awareness has also led her to volunteer with
+              Headspace and ANU Thrive, where she worked to raise awareness and
+              encourage wellbeing on campus.
+            </Text>
+            <Text py={3}>
+              With her passion for psychology and her desire to help others,
+              Claire strives to promote access to psychological services and
+              resources for everyone. She believes that by providing support and
+              resources to those in need, we can all work together to create a
+              more mentally healthy society.
+            </Text>
+          </Box>
+        </Flex>
+      </VStack>
+    </FullScreenSection>
+  );
 };
 
 export default Bio;
