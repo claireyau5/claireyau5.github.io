a8052f40bef3ed587aa0a275c3650bb78ef59b8a
terry-lws <terrylam299@gmail.com>
2023-05-23 05:30:25 +1000

    Add component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import Footer from "./components/Footer";
-import { AlertProvider } from "./context/alertContext";
 import BioSection from "./components/BioSection"
+import CertificateSection from "./components/CertificateSection";
 
 function App() {
   return (
     <ChakraProvider>
-      <AlertProvider>
         <main>
           <Header />
           <LandingSection />
           <BioSection />
+          <CertificateSection />
           <Footer />
         </main>
-      </AlertProvider>
     </ChakraProvider>
   );
 }
