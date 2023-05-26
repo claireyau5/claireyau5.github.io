11a648149ef1c43f70a8708d6980836f40f4ff82
terry-lws <terrylam299@gmail.com>
2023-05-26 20:52:13 +1000

    Add font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import Footer from "./components/Footer";
 import Bio from "./components/Bio"
 import Certificate from "./components/Certificate";
 
+const theme = extendTheme({
+  fonts: {
+    body: 'Roboto, sans-serif',
+    heading: 'Helvetica, sans-serif', 
+  },
+});
+
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
         <main>
           <Header />
           <LandingSection />
