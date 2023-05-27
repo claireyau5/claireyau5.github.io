01b6773dcaa84a2699a743f5abab4dffb99a7086
terry-lws <terrylam299@gmail.com>
2023-05-27 13:51:52 +1000

    Change font to Helvetica, sans-serif

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Certificate from "./components/Certificate";
 const theme = extendTheme({
   fonts: {
     body: 'Roboto, sans-serif',
-    heading: "Comic Sans MS, Comic Sans, cursive",
+    heading: 'Helvetica, sans-serif',
   },
 });
 
