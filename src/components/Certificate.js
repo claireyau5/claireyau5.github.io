d1f28204a868ba6c5757168252b86ee11e06dd96
terry-lws <terrylam299@gmail.com>
2023-05-28 00:58:21 +1000

    Refine section

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   Heading,
@@ -10,13 +10,45 @@ import {
   ModalBody,
   ModalContent,
   ModalOverlay,
+  IconButton,
   useDisclosure,
 } from "@chakra-ui/react";
-import { DownloadIcon } from "@chakra-ui/icons";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import FullScreenSection from "./FullScreenSection";
 
+const certificates = [
+  {
+    title: "CPR First Aid Certification",
+    imageSrc: require("../images/certificate.PNG"),
+  },
+  {
+    title:
+      "Aboriginal and Torres Strait Islander Mental Health First Aid Certificate",
+    imageSrc: require("../images/ATSI_certificate.png"),
+  },
+];
+
 const Certificate = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const totalCertificates = certificates.length;
+
+  const goToPrevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? totalCertificates - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalCertificates);
+  };
+
+  const currentCertificate = certificates[currentIndex];
+
+  const imgaeBoxWidth = 0.8;
+
+  const buttonBoxWidth = (1 - imgaeBoxWidth) / 2;
 
   return (
     <>
@@ -43,49 +75,82 @@ const Certificate = () => {
         color="#383842"
         px={8}
         py={10}
-        alignItems="flex-start"
+        alignItems="center"
         justifyContent="center"
         spacing={8}
       >
-        <VStack spacing={10}>
-          <Heading as="h1">Certification</Heading>
-          <Flex
-            justifyContent="center"
-            justifyItems="center"
-            alignItems="center"
-            gap={20}
-          >
-            <Flex w="50%" justifyContent="center">
-              <VStack spacing={5}>
-                <Text fontSize="3xl" fontWeight="600" textAlign="center">
-                  CPR First Aid Certification
+        <Box w="100%" position="relative">
+          <VStack spacing={10}>
+            <Heading as="h1">Certification</Heading>
+            <Flex justifyContent="center" alignItems="center" gap={20}>
+              <Box
+                w={buttonBoxWidth}
+                p={2}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <IconButton
+                  color="#e2e8f0"
+                  backgroundColor="#383842"
+                  icon={<ChevronLeftIcon />}
+                  aria-label="Previous Slide"
+                  onClick={goToPrevSlide}
+                  disabled={totalCertificates <= 1}
+                  position="absolute"
+                  left="0"
+                  top="50%"
+                  transform="translateY(-50%)"
+                  _hover={{ backgroundColor: "#e2e8f0", color: "#383842" }}
+                />
+              </Box>
+              <Box w="80%" p={2} justifyContent="center" alignItems="center">
+                <Image
+                  onClick={onOpen}
+                  cursor="pointer"
+                  src={currentCertificate.imageSrc}
+                  border="ridge #815a00 20px"
+                  maxW="100%"
+                  maxH="100%"
+                  objectFit="cover"
+                />
+                <Text fontWeight="600" textAlign="center" mt={5}>
+                  {currentCertificate.title}
                 </Text>
-              </VStack>
+                <Text fontSize="sm" fontWeight="600" textAlign="center" mt={2}>
+                  ({currentIndex + 1}/{totalCertificates})
+                </Text>
+                <Modal size="2xl" onClose={onClose} isOpen={isOpen} isCentered>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalBody>
+                      <Image src={currentCertificate.imageSrc} />
+                    </ModalBody>
+                  </ModalContent>
+                </Modal>
+              </Box>
+              <Box
+                w={buttonBoxWidth}
+                p={2}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <IconButton
+                  color="#e2e8f0"
+                  backgroundColor="#383842"
+                  icon={<ChevronRightIcon />}
+                  aria-label="Next Slide"
+                  onClick={goToNextSlide}
+                  disabled={totalCertificates <= 1}
+                  position="absolute"
+                  right="0"
+                  top="50%"
+                  transform="translateY(-50%)"
+                  _hover={{ backgroundColor: "#e2e8f0", color: "#383842" }}
+                />
+              </Box>
             </Flex>
-            <Box
-              //w={{ base: "none", lg: '40%' }}
-              w="50%"
-              p={2}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Image
-                onClick={onOpen}
-                cursor="pointer"
-                src={require("../images/certificate.PNG")}
-                border="ridge #815a00 20px"
-              />
-              <Modal size="2xl" onClose={onClose} isOpen={isOpen} isCentered>
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalBody>
-                    <Image src={require("../images/certificate.PNG")} />
-                  </ModalBody>
-                </ModalContent>
-              </Modal>
-            </Box>
-          </Flex>
-        </VStack>
+          </VStack>
+        </Box>
       </FullScreenSection>
     </>
   );
