1035a26c7afbbf74abab2f921beb8c9834b5f3fb
terry-lws <terrylam299@gmail.com>
2023-06-09 07:32:27 +1000

    Perform React practice

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,6 +6,15 @@ import WavyBreakLine from "./WavyBreakLine";
 const Bio = () => {
   const colorTheme = "#efebe4";
 
+  const firstPara =
+    "Claire Yau is a driven psychology enthusiast, passionate about understanding the complexities of the human mind and the impact of behavior on mental health. Her educational background includes a Bachelor of Psychology (Hons) degree from the Australian National University (ANU), where she gained a comprehensive understanding of the theories and applications of psychology.";
+  const secondPara =
+    "Currently working as a Patient Journey Coordinator at the Melbourne Psychiatry Centre, Claire is dedicated to providing patients with the resources and support they need to navigate the healthcare system and receive the best possible care. Her work has inspired her to pursue a career as a clinical psychologist, where she hopes to work directly with patients to provide evidence-based treatments and interventions.";
+  const thridPara =
+    "As an active volunteer with organizations like Raise Foundation and Lifeline, Claire puts her knowledge into practice by serving as a mentor and crisis supporter. Her commitment to promoting mental health awareness has also led her to volunteer with Headspace and ANU Thrive, where she worked to raise awareness and encourage wellbeing on campus.";
+  const forthPara =
+    "With her passion for psychology and her desire to help others, Claire strives to promote access to psychological services and resources for everyone. She believes that by providing support and resources to those in need, we can all work together to create a more mentally healthy society.";
+
   return (
     <>
       <FullScreenSection
@@ -42,24 +51,8 @@ const Bio = () => {
               />
             </Box>
             <Box w={{ xl: "50%", base: "100%" }}>
-              <Text py={3}>
-                Claire Yau is a driven psychology enthusiast, passionate about
-                understanding the complexities of the human mind and the impact
-                of behavior on mental health. Her educational background
-                includes a Bachelor of Psychology (Hons) degree from the
-                Australian National University (ANU), where she gained a
-                comprehensive understanding of the theories and applications of
-                psychology.
-              </Text>
-              <Text py={3}>
-                Currently working as a Patient Journey Coordinator at the
-                Melbourne Psychiatry Centre, Claire is dedicated to providing
-                patients with the resources and support they need to navigate
-                the healthcare system and receive the best possible care. Her
-                work has inspired her to pursue a career as a clinical
-                psychologist, where she hopes to work directly with patients to
-                provide evidence-based treatments and interventions.
-              </Text>
+              <Text py={3}>{firstPara}</Text>
+              <Text py={3}>{secondPara}</Text>
             </Box>
           </Flex>
           <Flex
@@ -70,21 +63,8 @@ const Bio = () => {
             gap={20}
           >
             <Box w={{ xl: "50%", base: "100%" }}>
-              <Text py={3}>
-                As an active volunteer with organizations like Raise Foundation
-                and Lifeline, Claire puts her knowledge into practice by serving
-                as a mentor and crisis supporter. Her commitment to promoting
-                mental health awareness has also led her to volunteer with
-                Headspace and ANU Thrive, where she worked to raise awareness
-                and encourage wellbeing on campus.
-              </Text>
-              <Text py={3}>
-                With her passion for psychology and her desire to help others,
-                Claire strives to promote access to psychological services and
-                resources for everyone. She believes that by providing support
-                and resources to those in need, we can all work together to
-                create a more mentally healthy society.
-              </Text>
+              <Text py={3}>{thridPara}</Text>
+              <Text py={3}>{forthPara}</Text>
             </Box>
             <Box
               w={{ xl: "50%", base: "100%" }}
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,49 +1,48 @@
 import React from "react";
 import { Heading, VStack, Text, Box } from "@chakra-ui/react";
 import lbg from "../images/lbg.jpeg";
-//import FullScreenSection from "./FullScreenSection";
+
+const name = "Claire Yau";
+const occupation = "Psychology Graduate";
+const introText =
+  "Passionate at becoming a clinical psychologist, where she can help individuals overcome mental health challenges and live fulfilling lives. With her educational background in psychology and her experience in patient care, Claire is dedicated to reducing the stigma surrounding mental health and promoting access to psychological services.";
 
 const LandingSection = () => {
   return (
     <Box
       display="flex"
-      flexDirection={{xl:"row", base:"column"}}
-      alignItems={{xl:"stretch", base:"center"}}
-      justifyContent={{xl:"space-between", base:"center"}}
-      textAlign={{xl:"start", base:"center"}}
+      flexDirection={{ xl: "row", base: "column" }}
+      alignItems={{ xl: "stretch", base: "center" }}
+      justifyContent={{ xl: "space-between", base: "center" }}
+      textAlign={{ xl: "start", base: "center" }}
       flex="1 1 auto"
       p={10}
       minHeight="100vh"
       color="white"
-      bg={`linear-gradient(to bottom, rgba(32,32,32,0.2), rgba(32,32,32,0.2)), url(${lbg})`}
+      bg={`linear-gradient(to bottom, rgba(32,32,32,0.25), rgba(32,32,32,0.25)), url(${lbg})`}
       bgSize="cover"
     >
       <VStack
         spacing={5}
         p={5}
-        alignItems={{xl:"flex-start", base:"center"}}
+        alignItems={{ xl: "flex-start", base: "center" }}
         justifyContent="top"
         position="relative"
       >
-        <Heading fontFamily={'Alex Brush, cursive'} fontSize={"9xl"}>Claire Yau</Heading>
-        <Heading fontSize={"4xl"}>Psychology Graduate</Heading>
+        <Heading fontFamily={"Alex Brush, cursive"} fontSize={"9xl"}>
+          {name}
+        </Heading>
+        <Heading fontSize={"4xl"}>{occupation}</Heading>
       </VStack>
       <VStack
-        w={{xl:"40%", base:"100%"}}
+        w={{ xl: "40%", base: "100%" }}
         spacing={5}
         p={5}
         alignItems="flex-end"
         justifyContent="flex-end"
         position="relative"
       >
-        <Text fontSize={"md"}>
-          Passionate at becoming a clinical psychologist, where
-          she can help individuals overcome mental health challenges and live
-          fulfilling lives. With her educational background in psychology and
-          her experience in patient care, Claire is dedicated to reducing the
-          stigma surrounding mental health and promoting access to psychological
-          services.
-        </Text>
+        <Text fontSize={"md"}>{introText}</Text>
       </VStack>
     </Box>
   );
