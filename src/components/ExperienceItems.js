08d7c1266a5128420ad0bd4d519208ac0d234404
terry-lws <terrylam299@gmail.com>
2023-09-14 16:27:00 +1000

    Minor code format change

--- src/components/ExperienceItems.js
+++ src/components/ExperienceItems.js
@@ -18,7 +18,7 @@ const ExperienceItems = ({
 }) => {
   return (
     <Flex
-      flexDirection={{md:"row", base:"column"}}
+      flexDirection={{ md: "row", base: "column" }}
       justifyContent="center"
       justifyItems="center"
       alignItems="center"
@@ -26,12 +26,21 @@ const ExperienceItems = ({
       borderRadius="25px"
       boxShadow="5px 5px 10px #0f2849"
     >
-      <Box w={{md:"30%", base:"50%"}} py={4} px={[2,4]} justifyContent="center" alignItems="center">
+      <Box
+        display={"flex"}
+        w={{ md: "30%", base: "50%" }}
+        py={4}
+        px={[2, 4]}
+        justifyContent="center"
+        alignItems="center"
+      >
         <Image src={imageSrc} />
       </Box>
-      <Box w={{md:"70%", base:"100%"}} color="black">
+      <Box w={{ md: "70%", base: "100%" }} color="black">
         <VStack spacing={5} p={4} align="left">
-          <Text color="#575757" fontSize={"sm"}>{date}</Text>
+          <Text color="#575757" fontSize={"sm"}>
+            {date}
+          </Text>
           <Heading size="md">{position}</Heading>
           <Heading size="sm" color="#575757" style={{ marginTop: "4px" }}>
             {company}
